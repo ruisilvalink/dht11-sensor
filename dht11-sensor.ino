1be57714988a09d690d05314880d40a456628285
Rui Silva <38074835+ruisilvalink@users.noreply.github.com>
2020-04-25 22:54:29 +0100

    Persist configuration

--- dht11-sensor.ino
+++ dht11-sensor.ino
@@ -1,14 +1,16 @@
+#include <LittleFS.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <dht11.h>
+#include "FS.h"
 
-#define DHT11PIN 0
+const int DHT11_PIN = 0; // ESP8666 pin D3
+const int ON_SWITCH_PIN = 5; // ESP8666 pin D1
+
+int onSwitchState = 0; // Start in setup mode
 
 ESP8266WebServer server(80);
 dht11 DHT11;
-
-boolean setupCompleted = false;
-boolean initialized = false;
 
 String softApName = "DHT11-sensor";
 String softApPassword = "";
@@ -18,23 +20,30 @@ String wifiPassword;
 
 String serverHost;
 String serverPort;
-String serverPath";
+String serverPath;
 String serverToken;
 
 const String homePage = "\
 <html>\
   <head>\
     <title>dht11-sensor setup</title>\
+    <style>\
+      label {\
+        display: block;\
+        margin-top: 10px;\
+      }\
+    </style>\
   </head>\
   <body>\
     <h1>Setup</h1><br>\
     <form method=\"post\" enctype=\"application/x-www-form-urlencoded\" action=\"/setup\">\
-      Wifi Name: <input type=\"text\" name=\"wifiName\"><br>\
-      Wifi Passwordt: <input type=\"text\" name=\"wifiPassword\"><br>\
-      Server Host: <input type=\"text\" name=\"serverHost\"><br>\
-      Server Port: <input type=\"text\" name=\"serverPort\"><br>\
-      Server Path: <input type=\"text\" name=\"serverPath\"><br>\
-      Server Token: <input type=\"text\" name=\"serverToken\"><br>\
+      <label>Wifi Name:</label><input type=\"text\" name=\"wifiName\"><br>\
+      <label>Wifi Password:</label><input type=\"password\" name=\"wifiPassword\"><br>\
+      <label>Server Host:</label><input type=\"text\" name=\"serverHost\"><br>\
+      <label>Server Port:</label><input type=\"text\" name=\"serverPort\"><br>\
+      <label>Server Path:</label><input type=\"text\" name=\"serverPath\"><br>\
+      <label>Server Token:</label><input type=\"text\" name=\"serverToken\"><br>\
+      <br>\
       <input type=\"submit\" value=\"Submit\">\
     </form>\
   </body>\
@@ -43,10 +52,16 @@ const String homePage = "\
 void setup()
 {
   Serial.begin(115200);
+  delay(3000);
   Serial.println();
+  
+  LittleFS.begin();
 
-  boolean result = WiFi.softAP(softApName, softApPassword);
-  Serial.println( result ? "Ready" : "Failed!");
+  loadFSConfig();
+
+  pinMode(ON_SWITCH_PIN, INPUT);
+
+  startWifiAP();
   
   server.on("/", homeController);
   server.on("/setup", setupController);
@@ -55,22 +70,53 @@ void setup()
 
 void loop()
 {
-  if (!setupCompleted) {
-    server.handleClient();
-  } else  if (!initialized){
-    delay(5000);
-    server.stop();
-    connectWifi();
-    initialized = true;
-  } else {
-    if (DHT11.read(DHT11PIN) == DHTLIB_OK) 
-    {
-      save(DHT11.humidity, DHT11.temperature);
-    } else {    
-      Serial.println("Sensor read error!");
+  int currentOnSwitchState = digitalRead(ON_SWITCH_PIN);
+  if (currentOnSwitchState == HIGH) {
+    if (currentOnSwitchState != onSwitchState) {
+      startWifiSTA();
     }
-    delay(5000);
+    readSensor();
+  } else {
+    if (currentOnSwitchState != onSwitchState) {
+      startWifiAP();
+    }
+    server.handleClient();
   }
+  onSwitchState = currentOnSwitchState;
+}
+
+void loadFSConfig() {
+  File file = LittleFS.open("/config.txt", "r");
+  if (!file) {
+    Serial.println("Config not found.");
+    return;
+  }
+  wifiName = file.readStringUntil('\n');
+  wifiPassword = file.readStringUntil('\n');
+  serverHost = file.readStringUntil('\n');
+  serverPort = file.readStringUntil('\n');
+  serverPath = file.readStringUntil('\n');
+  serverToken = file.readStringUntil('\n');
+  Serial.println("wifiName: " + wifiName);
+  Serial.println("wifiPassword: " + wifiPassword);
+  Serial.println("serverHost: " + serverHost);
+  Serial.println("serverPort: " + serverPort);
+  Serial.println("serverPath: " + serverPath);
+  Serial.println("serverToken: " + serverToken);
+}
+
+void saveFSConfig() {
+  File file = LittleFS.open("/config.txt", "w");
+  if (!file) {
+    Serial.println("Config not found.");
+    return;
+  }
+  file.print(wifiName + '\n');
+  file.print(wifiPassword + '\n');
+  file.print(serverHost + '\n');
+  file.print(serverPort + '\n');
+  file.print(serverPath + '\n');
+  file.print(serverToken + '\n');
 }
 
 void homeController() {
@@ -91,37 +137,63 @@ void setupController() {
     serverPort = server.arg("serverPort");
     serverPath = server.arg("serverPath");
     serverToken = server.arg("serverToken");
-    setupCompleted = true;
+    saveFSConfig();
     server.send(200, "text/plain", "Setup Done");
   }
 }
 
-void connectWifi() {
+void startWifiAP() {
+  WiFi.disconnect(true);
+  
+  boolean result = WiFi.softAP(softApName, softApPassword);
+  Serial.println( result ? "Ready" : "Failed!");
+}
+
+void startWifiSTA() {
+  WiFi.softAPdisconnect(true);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifiName, wifiPassword);
 
   Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
+  int timeout = 10000;
+  while (WiFi.status() != WL_CONNECTED && timeout > 0)
   {
-    delay(500);
+    int waitTime = 1000;
+    delay(waitTime);
+    timeout -= waitTime;
     Serial.print(".");
   }
   Serial.println();
 
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Not connected!");
+  } else {
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void readSensor() {
+  if (DHT11.read(DHT11_PIN) == DHTLIB_OK) 
+  {
+    Serial.println();
+    
+    Serial.print("Humidity (%): ");
+    Serial.println(DHT11.humidity, 2);
+  
+    Serial.print("Temperature (C): ");
+    Serial.println(DHT11.temperature, 2);
+    
+    save(DHT11.humidity, DHT11.temperature);
+  } else {    
+    Serial.println("Sensor read error!");
+  }
+  delay(5000);
 }
 
 void save(float humidity, float temperature) 
 {
-  Serial.println();
-  
-  Serial.print("Humidity (%): ");
-  Serial.println(humidity, 2);
-
-  Serial.print("Temperature (C): ");
-  Serial.println(temperature, 2);
-    
   String content = "token=" + serverToken + "&humidity=" + String(humidity) + "&temperature=" + String(temperature);
   
   WiFiClient client;
